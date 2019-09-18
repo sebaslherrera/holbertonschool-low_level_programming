9504d1b9350e72b0d3b81ead22143f99707cbefe
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-18 19:56:56 +0000

    Added betty styles

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
 #include <unistd.h>
 
+
+/**
+ * main - Print a text in stderr with write() function
+ *
+ * Return: 1 successful
+ */
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str, sizeof(str) - 1);	
-	
+	write(2, str, sizeof(str) - 1);
+
 	return (1);
 }
