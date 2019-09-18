5b02bf34a9c5d1110a01f83c7e6633c3a8cac064
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-18 17:47:23 +0000

    Printing to the stderr with write() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Print a string with a line in STDOUT
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
