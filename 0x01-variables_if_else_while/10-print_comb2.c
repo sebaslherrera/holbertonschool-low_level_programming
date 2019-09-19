aad4ad9e58c34bf018396877c982cd594ae00c22
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-19 21:20:18 +0000

    Print number 00 to 99 with two for loops

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers 00 to 99 separated with ',' and space
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int i, j;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		for(j = 48; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if(i != 57 || j != 57){	
+				putchar(',');
+				putchar(' ');
+			}
+		}
+			
+	}
+	
+
+	putchar('\n');
+	return (0);
+}
+
