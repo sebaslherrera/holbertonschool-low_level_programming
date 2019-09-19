e3caf5bfd2e3170839b22b613653c63ebd821233
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-19 20:11:07 +0000

    Print numbers 0 to 9 with putchar()

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print number of 0 to 10
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
