be87e40e4a2cfd7bfde34429f964dc9f865b37bc
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-23 13:45:16 +0000

    All the files for the tasks created and print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+int main(void)
+{
+        char c[] = "Holberton";
+        int i;
+        for (i = 0; i < 9; i++){
+                _putchar(c[i]);
+        }
+        _putchar('\n');
+
+        return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# This is the functions and nested loops project in C
