8321aeb69df70050acc5eff63e38c8631f59e24d
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-23 16:48:31 +0000

    Print the alphabet x10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Print all the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -4,14 +4,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif
