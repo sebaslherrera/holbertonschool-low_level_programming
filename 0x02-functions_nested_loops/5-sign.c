03da45033faf287d718f073ef735fe91e37120f6
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-23 21:51:52 +0000

    Print the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_sign - Check if is a letter, lowercase or uppercase
+ *
+ * @c is the representation of char in ascii code
+ * Return: 1 letter and 0 to otherwise
+ */
+int print_sign(int n)
+{
+	if(n > 0){
+		_putchar('+');
+		return (1);
+	}else if (n == 0){
+		_putchar('0');
+		return (0);
+	}else {
+		_putchar('-');
+		return (-1);
+	}
+}
