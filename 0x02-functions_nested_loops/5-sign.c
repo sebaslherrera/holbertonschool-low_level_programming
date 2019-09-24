a0346d93274fc879ccc9083cd7f7f19b8fdf359e
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-24 13:55:29 +0000

    mend

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,25 @@
 #include "holberton.h"
 
 /**
- * print_sign - Check if is a letter, lowercase or uppercase
+ * print_sign - Check if is a letter, lowercase or uppercase *
+ * @n: is the representation of char in ascii code
  *
- * @c is the representation of char in ascii code
  * Return: 1 letter and 0 to otherwise
  */
 int print_sign(int n)
 {
-	if(n > 0){
+	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
-	}else if (n == 0){
+	}
+	else if (n == 0)
+	{
 		_putchar('0');
 		return (0);
-	}else {
+	}
+	else
+	{
 		_putchar('-');
 		return (-1);
 	}
