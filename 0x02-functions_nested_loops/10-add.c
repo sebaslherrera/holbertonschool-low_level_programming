de269ff7b1acc87b0bb2476078f7cbeb43ba733e
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-24 14:28:26 +0000

    Sum two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - Print the last digit of a number *
+ *
+ * @n: Integer parameter
+ * @y: Integer parameter
+ * Return: n (int)
+ */
+int add(int x, int y)
+{
+	return x + y;
+}
