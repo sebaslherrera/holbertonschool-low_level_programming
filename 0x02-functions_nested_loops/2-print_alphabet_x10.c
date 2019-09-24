5558f477e84e4ef32da63cde4122c293adf042a8
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-24 13:49:33 +0000

    mend

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * print_alphabet - Print all the alphabet
+ * print_alphabet_x10 - Print all the alphabet
  */
 void print_alphabet_x10(void)
 {
 	char a;
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		a = 'a';
