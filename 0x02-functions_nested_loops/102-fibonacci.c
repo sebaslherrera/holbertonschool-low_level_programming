afa52e2e9f60afdb623a435c646aa7b73f9e6987
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-24 22:17:11 +0000

    Print the first 50 fibonnaci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print the ...
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	long long  x1 = 0, x2 = 1, next = 0, n = 49;
+
+	printf("%lli, ", x2);
+
+	while (n != 0){
+		next = x1 + x2;
+		x1 = x2;
+		x2 = next;
+		printf("%lli", next);
+		if(n > 1){
+			printf(", ");
+		}
+		n--;
+	}
+	putchar('\n');
+}
