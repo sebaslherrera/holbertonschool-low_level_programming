727784c5a2f1af5906dea002a25586bd3e115c29
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-24 13:50:52 +0000

    mend

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * _islower - Print if is lower or upper case
+ * _islower - Print if is lower or upper case *
+ * @c: is the representation of char in ascii code
  *
- * @c is the representation of char in ascii code
  * Return: 1 lowercase and 0 to lowercase
  */
 int _islower(int c)
