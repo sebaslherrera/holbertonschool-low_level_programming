07cfe18875c51acbaf1a434da08ea44a95be3247
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-26 19:21:56 +0000

    Draw a triangle on terminal

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - Draw a triangle on terminal
+ * @size: Integer parameter
+ */
+void print_triangle(int size)
+{
+	int i, x = 1, n = size - 1, h, z;
+	
+	if (size < 1)
+	{
+		_putchar('\n');
+	}
+	else{
+		for (i = 0; i < size; i++)
+		{
+			h = n;
+			z = x;
+			while (h)
+			{
+				_putchar(' ');
+				h--;
+			}
+			n--;
+			while (z)
+			{
+				_putchar('#');
+				z--;
+			}
+			x++;
+			_putchar('\n');
+		}
+
+	}
+}
