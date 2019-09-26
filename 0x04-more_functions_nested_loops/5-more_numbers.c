3357baca3280de64bfb1c984ba70b79d5b8f71af
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-26 15:41:18 +0000

    Print more numbers several times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_numbers_n - Print numbers from 0 to 14
+ *
+ */
+void print_numbers_n(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 14; ++i)
+	{
+		if (i < 10)
+		{
+			_putchar(i + '0');
+		}
+		else
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+	}
+}
+
+/**
+ * more_numbers - Print numbers 0 to 14 ten times
+ *
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; ++i)
+	{
+		print_numbers_n();
+		_putchar('\n');
+	}
+}
