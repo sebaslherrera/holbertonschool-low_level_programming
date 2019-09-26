ccf75e72532fecd076092774441c6a1ca8aeb17f
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-26 15:54:30 +0000

    Draw a straight line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_line - Draw a straight line in the terminal
+ * @n: Integer parameter
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; ++i)
+	{
+		if(n == 0)
+			break;
+		_putchar('_');
+	}
+	_putchar('\n');
+}
