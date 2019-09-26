2dd44cb3459993469406f2280109a3acf3c22eb1
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-26 15:27:01 +0000

    Print most numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Print numbers from 0 to 8
+ *
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; ++i){
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
