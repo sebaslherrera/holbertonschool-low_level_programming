1c3f3c3b8940478915018309be600d397510345d
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-26 18:06:33 +0000

    Print a square on terminal

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_square - Draw a square on the terminal
+ * @size: Integer parameter
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
