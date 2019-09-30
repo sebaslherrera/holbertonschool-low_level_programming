9d16f3cfb5b90500400db6b63009a42fdb1f2457
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-30 20:24:52 +0000

    Length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Length of the string
+ *
+ * @s: Pointer of char variable
+ * Return: Integer value
+ */
+int _strlen(char *s)
+{
+	char *t;
+	int counter = 0;
+
+	for (t = s; *t != '\0'; t++)
+	{
+		counter++;
+	}
+
+	return counter;
+}
