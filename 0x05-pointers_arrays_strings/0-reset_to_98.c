0b46eba34a25a9f26c33da9a79e80b9b5441ab08
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-09-30 19:59:41 +0000

    Reset a varible passed by value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Modifies a variable from outside the function it is declared
+ *
+ * @n: Pointer parameter
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
