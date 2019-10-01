acd8f160fdda40e8dae7aa89fa33b4c5744603c4
sebas119_laptop <lopez.sebastian@correounivalle.edu.co>
2019-10-01 17:49:11 -0500

    Testing files

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - check the code for Holberton School students.
  *
@@ -9,22 +9,46 @@
 int main(void)
 {
     int n;
-
-    n = _atoi("-2147483648");
-    printf("%d\n", n);
-    n = _atoi("2147483647");
-    printf("%d\n", n);
-    n = _atoi("          ------++++++-----+++++--06465");
-    printf("%d\n", n);
-    n = _atoi("214748364");
-    printf("%d\n", n);
-    n = _atoi("0");
-    printf("%d\n", n);
-    n = _atoi("Suite 402");
-    printf("%d\n", n);
-    n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", n);
-    n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", n);
+	n = _atoi("214748364");
+	printf("%d\n", n);
+	n = _atoi("0");
+	printf("%d\n", n);
+	n = _atoi("Suite 402");
+	printf("%d\n", n);
+	n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+	printf("%d\n", n);
+	n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+	printf("%d\n", n);
+	n = _atoi("-2147483648");
+	printf("%d\n", n);
+	n = atoi("-2147483648");
+	printf("%d atoi\n", n);
+	n = _atoi("2147483647");
+	printf("%d\n", n);
+	n = _atoi("2147483648");
+	printf("%d\n", n);
+	n = atoi("2147483648");
+	printf("%d atoi\n", n);
+	n = _atoi("121212121212121212");
+	printf("%d\n", n);
+	n = atoi("121212121212121212");
+	printf("%d atoi\n", n);
+	n = _atoi("1.2.3.4.5");
+	printf("%d\n", n);
+	n = atoi("1.2.3.4.5");
+	printf("%d atoi\n", n);
+	n = _atoi("-1.2.3.4.5");
+	printf("%d\n", n);
+	n = atoi("-1.2.3.4.5");
+	printf("%d atoi\n", n);
+	n = _atoi("-2147483649");
+	printf("%d\n", n);
+	n = atoi("-2147483649");
+	printf("%d atoi\n", n);
+	n = _atoi("123456789.123456789");
+	printf("%d\n", n);
+	n = atoi("123456789.123456789");
+	printf("%d atoi\n", n);	
+	
     return (0);
 }
