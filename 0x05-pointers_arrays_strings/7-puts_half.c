eb699c02a9ba145a43b14a9a65bcc35b941977af
Sebas Vagrant <lopez.sebastian@correounivalle.edu.co>
2019-10-01 00:32:27 +0000

    Print the half right of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts_half - Prints half right of a string
+ *
+ * @str: Pointer of char[] variable
+ */
+void puts_half(char *str)
+{
+	char *t; 
+	int counter = 0, middle;
+	int i; 
+
+	for (t = str; *t != '\0'; t++)
+		counter++;
+	
+	middle = (counter % 2 != 0) ? ((counter - 1) / 2) : counter / 2;
+	for (i = middle; i < counter; i ++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
