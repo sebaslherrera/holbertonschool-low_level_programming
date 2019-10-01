6d03ecab3b00dd6111fabeeb4768bfc4b5930999
sebas119_laptop <lopez.sebastian@correounivalle.edu.co>
2019-10-01 17:51:19 -0500

    Testing files 2

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+int _atoi(char *s)
+{
+        if (*s == '\0')
+                return (0);
+
+        unsigned int ans = 0;
+        int i = 0, sign = 1;
+
+
+        while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+        {
+                if (s[i] == '-')
+                        sign *= -1;
+                i++;
+        }
+        sign *= -1;
+        while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+        {
+                ans = ans * 10 - (s[i] - '0');
+                i++;
+        }
+        return (ans * sign);
+}
