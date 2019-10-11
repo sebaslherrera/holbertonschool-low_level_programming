7f761f56b1f6eba5ca617cde0eb7c34295473cab
laura Pc <laura.villan@correounivalle.edu.co>
2019-10-10 20:36:19 -0500

    mend-noedit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,7 @@
 #include "holberton.h"
 
-/**
- * is_prime_number_mod - Recursive function to check prime numbers
- * @n: Given number
- * @i: Variable counter
- * @j: flag variable
- * Return: 1 is prime or 0 not prime
- */
-int is_prime_number_mod(int n, int i, int j)
-{
-	if (i >= n)
-		return (1);
-	if (j == 1)
-		return (0);
+int is_prime_number_mod(int n, int i);
 
-	if (n % i == 0)
-		return (is_prime_number_mod(n, i, 1));
-	return (is_prime_number_mod(n, i + 1, j));
-}
 /**
  * is_prime_number - Check if the given number is prime
  * @n: Number to check
@@ -27,7 +11,22 @@ int is_prime_number(int n)
 {
 	if (n < 2)
 		return (0);
-	int i = 2, j = 0;
+	int i = 2;
 
-	return (is_prime_number_mod(n, i, j));
+	return (is_prime_number_mod(n, i));
+}
+
+/**
+ * is_prime_number_mod - Recursive function to check prime numbers
+ * @n: Given number
+ * @i: Variable counter
+ * Return: 1 is prime or 0 not prime
+ */
+int is_prime_number_mod(int n, int i)
+{
+	if (n == i)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	return (is_prime_number_mod(n, i + 1));
 }
