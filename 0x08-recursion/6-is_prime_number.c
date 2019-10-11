f1c21396146a7e7cd34cadc537d72b173253a828
laura Pc <laura.villan@correounivalle.edu.co>
2019-10-10 19:01:29 -0500

    Check if the number is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number_mod - Recursive function to check prime numbers
+ * @n: Given number
+ * @i: Variable counter
+ * @j: flag variable
+ * Return: 1 is prime or 0 not prime
+ */
+int is_prime_number_mod(int n, int i, int j)
+{
+	if (i >= n)
+		return (1);
+	if (j == 1)
+		return (0);
+
+	if (n % i == 0)
+		return (is_prime_number_mod(n, i, 1));
+	return (is_prime_number_mod(n, ++i, j));
+}
+/**
+ * is_prime_number - Check if the given number is prime
+ * @n: Number to check
+ * Return: 1 is prime or 0 not prime
+ */
+int is_prime_number(int n)
+{
+	if (n == 1 || n <= 0)
+		return (0);
+	int i = 2, j = 0;
+
+	return (is_prime_number_mod(n, i, j));
+}
