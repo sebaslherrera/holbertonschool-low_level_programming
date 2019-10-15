d6223f1cc334abbb25b4561b8ca19c6d6df89467
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-15 21:26:17 +0000

    Some fixes of malloc 2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,15 @@ int **alloc_grid(int width, int height)
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
+	{
 		ptr[i] = (int *) malloc(width * sizeof(int));
+
+		if (ptr[i] == NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}
+	}
 
 	if (*ptr == NULL)
 	{
