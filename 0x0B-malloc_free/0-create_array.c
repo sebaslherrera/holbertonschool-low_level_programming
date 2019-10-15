eb27e36c7fabdedd61fd82eea06d58e22e91871f
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-15 15:34:02 +0000

    Create a dinamically array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - Function that creates arrays of chars
+ * @size: Size of the char
+ * @c: Char to be initialized
+ * Return: Array of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptc;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ptc = (char *) malloc(size * (sizeof(char)));
+
+	for (i = 0; i < size; i++)
+		ptc[i] = c;
+
+	return (ptc);
+}
