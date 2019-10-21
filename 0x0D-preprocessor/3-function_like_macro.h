bdbfb4f805670d30d41e02979ca8b67f8dd8f3c5
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-21 15:52:24 +0000

    Protected files the guard

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,4 @@
+#ifndef ABS_MACRO_H
+#define ABS_MACRO_H
 #define ABS(X) ((X < 0) ? (-X) : (X))
+#endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,1 +1,4 @@
+#ifndef MACRO_SUM_H
+#define MACRO_SUM_H
 #define SUM(X, Y) ((X + Y))
+#endif
