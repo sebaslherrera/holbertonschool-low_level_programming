ff9e28e5a05157e7831e559628acdae4b17cf515
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-21 19:38:14 +0000

    Dog structure created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Describe a dog and some additional data
+ * @name: String name of the dog
+ * @age: float age of the dog
+ * @owner: String name of the owner of the dog
+ *
+ * Description: Dog and owner data
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
