94311594f758db432a2b2dff69693e819a567ddf
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-21 15:05:24 +0000

    Some style fixes and ABS macro function

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * main - Prints the name of the file
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
+	printf("%s\n", __FILE__);
 
-	printf("%s\n", __FILE__ );
 	return (0);
 }
