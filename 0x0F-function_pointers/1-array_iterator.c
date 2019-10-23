1fef2a72a2d60f15ab92b2d66e878184c5d1a970
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-23 17:14:04 +0000

    Missing file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Print the data of an array of integers
+ * @array: Array of integers
+ * @size: Unsigned long data type for the size of the array
+ * @action: Function that performs the print, receives an integer value
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *myarray;
+	unsigned long i;
+
+	myarray = array;
+
+	for (i = 0; i < size; i++)
+	{
+		action(myarray[i]);
+	}
+}
