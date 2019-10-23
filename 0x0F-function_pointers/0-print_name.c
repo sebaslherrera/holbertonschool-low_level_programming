e6cf51ba3170b95412c1cbff4880f8f2c6b1b0f3
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-23 14:56:12 +0000

    Function that prints a name (another function)

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Execute the function passed by argument
+ * @name: String name
+ * @f: Function void that receives as argument a string
+ *
+ * Return: nothing;
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
