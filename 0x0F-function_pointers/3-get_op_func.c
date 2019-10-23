815903fe8cc36fe27b3a197772316b3e1a8ac4d4
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-23 20:04:19 +0000

    Program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+
+/**
+ * *get_op_func - Select the correct function to perform
+ * the operation given
+ * @s: String given
+ *
+ * Return: Return a function pointer of which returns an integer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i <= 4)
+	{
+		if (*ops[i].op == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+
+	return (NULL);
+
+}
