bcd1e280d8b248add0c7c0066abb6359880fe2e8
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-25 15:07:23 +0000

    Print all the strings passed

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - Prints strings
+ * @separator: String constant
+ * @n: Number of arguments strings ...
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *temp;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		temp = va_arg(args, char *);
+		if (temp != NULL)
+			printf("%s", temp);
+		else
+			printf("(nil)");
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+}
