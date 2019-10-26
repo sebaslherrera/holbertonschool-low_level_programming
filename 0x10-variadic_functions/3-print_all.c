ac0c41423e9cc09804602724f9aa8a423fa70ee0
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-26 19:20:45 +0000

    Added a structure

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
+void print_char(va_list args, char *separator);
+void print_integer(va_list args, char *separator);
+void print_float(va_list args, char *separator);
+void print_string(va_list args, char *separator);
 
 /**
  * print_all - Print all of data passed
@@ -15,38 +17,85 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0;
+	unsigned int i = 0, j = 0;
 	char *sep = "";
+	td types[] = {
+		{'c', print_char},
+		{'i', print_integer},
+		{'f', print_float},
+		{'s', print_string}
+	};
 
 	va_start(args, format);
 
 	while (format[i] != '\0')
 	{
-		switch (format[i])
+		j = 0;
+		while (j < 4)
 		{
-			case 'c':
-				printf("%s", sep);
-				printf("%c", va_arg(args, int));
+			if (format[i] == types[j].type)
+			{
+				types[j].data_print(args, sep);
 				sep = ", ";
-				break;
-			case 'i':
-				printf("%s", sep);
-				printf("%d", va_arg(args, int));
-				sep = ", ";
-				break;
-			case 'f':
-				printf("%s", sep);
-				printf("%f", va_arg(args, double));
-				sep = ", ";
-				break;
-			case 's':
-				printf("%s", sep);
-				printf("%s", va_arg(args, char *));
-				sep = ", ";
-				break;
+			}
+			j++;
 		}
 		i++;
 	}
 	va_end(args);
 	printf("\n");
+}
+
+
+
+/**
+ * print_char - Prints a char value
+ * @args: Argument passed
+ * @separator: separator of data for better visualization
+ *
+ * Return: nothing
+ */
+void print_char(va_list args, char *separator)
+{
+	printf("%s", separator);
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_integer - Prints an integer value
+ * @args: Argument passed
+ * @separator: separator of data for better visualization
+ *
+ * Return: nothing
+ */
+void print_integer(va_list args, char *separator)
+{
+	printf("%s", separator);
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - Prints a float value
+ * @args: Argument passed
+ * @separator: separator of data for better visualization
+ *
+ * Return: nothing
+ */
+void print_float(va_list args, char *separator)
+{
+	printf("%s", separator);
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - Prints a string value
+ * @args: Argument passed
+ * @separator: separator of data for better visualization
+ *
+ * Return: nothing
+ */
+void print_string(va_list args, char *separator)
+{
+	printf("%s", separator);
+	printf("%s", va_arg(args, char *));
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,19 @@
 
 #include <stdarg.h>
 
+/**
+ * struct typedata - Describe a specifier for a data type
+ * @type: id that identifies the type of data
+ * @data_print: Function that receives a data and a separator
+ * Description: type and function data
+ */
+typedef struct typedata
+{
+	char type;
+	void (*data_print)(va_list, char *);
+} td;
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
