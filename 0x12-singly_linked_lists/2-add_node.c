bfb0834dc125020a98a5976f4e1e8823e588166e
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-31 17:07:54 +0000

    Fixed

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include "string.h"
+
+/**
+ * add_node -
+ * @head:
+ * @str:
+ *
+ * Return:
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *head;
+	list_t *new;
+	list_t end = {NULL, 0, NULL};
+}
