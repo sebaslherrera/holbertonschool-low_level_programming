d6d89e3cbba53d73e77d3899a3e1d109023fdc6a
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-31 16:53:01 +0000

    Count the number of elements in the list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - Count number of elements in a list
+ * @h: Data of type linked list
+ *
+ * Return: Size of linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
