07e0d15a740b8b1272af365a6cad153bdc8f2e5d
sebas119 <lopez.sebastian@correunivalle.edu.co>
2019-11-06 02:51:58 +0000

    Files of advanced

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,13 +2,13 @@
 
 /**
  * print_listint_safe - Prints all the elements of a linked list
- * @h: Head of the linked list
+ * @head: Head of the linked list
  *
  * Return: Number of nodes (size_t)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *temp = head;
+	const listint_t *temp = head;
 	size_t i = 0;
 
 	if (temp == NULL)
