0e72290c5bb434b1896889ac1c08f04b3d93bd5c
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-11-12 17:21:42 +0000

    Reads a text file and prints to the stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,38 @@
 #include "holberton.h"
 
 /**
- * read_textfile -
- * @filename:
- * @letters:
+ * read_textfile - Reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: File name of the archive
+ * @letters: Number of letters stored in the buffer
  *
- * Return:
+ * Return: The size of the letters readed and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd, readData;
+	char *buffer;
+	ssize_t ans;
 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+		return (0);
+
+	readData = read(fd, buffer, letters);
+
+	if (readData < 0)
+		return (0);
+	ans = write(1, buffer, readData);
+
+	if ((ans < 0) | (ans != readData))
+		return (0);
+	free(buffer);
+	return (ans);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,8 +1,9 @@
 #ifndef IO_H
 #define IO_H
 
-#include <stdlib.>
+#include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
