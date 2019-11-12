3ef9a179f1ff0790a24ba9ff9127989f23e6d1cd
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-11-12 16:52:42 +0000

    All the files has been created

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * read_textfile -
+ * @filename:
+ * @letters:
+ *
+ * Return:
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,9 @@
+#ifndef IO_H
+#define IO_H
+
+#include <stdlib.>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
