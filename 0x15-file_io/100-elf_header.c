71eba43f11989ba9d6207fe1f3ecd61d26023000
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-11-13 01:14:31 +0000

    Started

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <elf.h>
+
+/**
+ * main - Displays information contained in the ELF header
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	return (0);
+}
