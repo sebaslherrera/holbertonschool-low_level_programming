682b0c984f75efe6801520f4001fc10af3e7763f
sebas119 <lopez.sebastian@correunivalle.edu.co>
2019-12-15 20:25:45 +0000

    Insert a new node in the given index fixed 6

--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -12,6 +12,8 @@ int main(void)
 {
     dlistint_t *head;
 
+    printf("\n\n");
+    printf("FIRST TEST (tail insertion)\n");
     head = NULL;
     add_dnodeint_end(&head, 0);
     add_dnodeint_end(&head, 1);
@@ -23,9 +25,48 @@ int main(void)
     add_dnodeint_end(&head, 1024);
     print_dlistint(head);
     printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 7, 4096);
+    insert_dnodeint_at_index(&head, 8, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
+
+    printf("\n\n");
+    printf("SECOND TEST (empty list)\n");
+    head = NULL;
+    print_dlistint(head);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 0, 4096);
+    print_dlistint(head);
+    free_dlistint(head);
+    head = NULL;
+
+    printf("\n\n");
+    printf("THIRD TEST (middle)\n");
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 5, 4096);
+    print_dlistint(head);
+    free_dlistint(head);
+    head = NULL;
+
+    printf("\n\n");
+    printf("FOURTH TEST (another test)\n");
+    head = NULL;
+    print_dlistint(head);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 10, 4096);
+    print_dlistint(head);
+    free_dlistint(head);
+    head = NULL;
+
     return (EXIT_SUCCESS);
 }
