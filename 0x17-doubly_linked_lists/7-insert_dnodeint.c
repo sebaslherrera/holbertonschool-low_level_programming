a6f03ebc3c73f91bf494b1135a1199f0f327fada
sebas119 <lopez.sebastian@correunivalle.edu.co>
2019-12-15 00:36:58 +0000

    Insert a new node in the given index fixed 2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,6 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->prev = NULL;
 		new->next = NULL;
+		*h = new;
 		return (new);
 	}
 	if (idx == 0 && head != NULL)
