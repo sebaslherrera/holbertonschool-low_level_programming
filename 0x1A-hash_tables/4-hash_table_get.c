50c84b96feb7ff5442296c592257c9631b0405a0
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-18 23:47:50 +0000

    Retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,23 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get -
- *
- * Return: 
+ * hash_table_get - a
+ * @ht: a
+ * @key: a
+ * Return: a
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	const hash_table_t *table = ht;
+	unsigned long int index;
+	hash_node_t *head = NULL;
 
+	if (table == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, table->size);
+	head = table->array[index];
+	if (head == NULL)
+		return (NULL);
+	return (head->value);
 }
