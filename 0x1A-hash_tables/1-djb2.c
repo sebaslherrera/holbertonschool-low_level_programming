af5bda4e8da3cf37cf85431d89b701fb1f15e2e8
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-18 20:55:31 +0000

    Hash function

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,23 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_djb2 - Hash function algorithm from computer scientist
+ * Dan Bernstein. It uses bit manipulation and prime numbers to
+ * create a hash index from a string.
+ * @str: String given
+ *
+ * Return: unsigned long int code
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
